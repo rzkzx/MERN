880df8ac66cb6505f72b4b8cc35b627abc500efd
Roman Zidan Ramadhan <romanzidanramadhan@gmail.com>
2020-05-05 23:44:25 +0800

    Revert "Update Details Page"
    This reverts commit 37fca18715b7e18b9cc8a2a51e8d116b58a619a8.

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
 
-import Fade from 'react-reveal/Fade';
-
 import Header from 'parts/Header';
 import PageDetailTitle from 'parts/PageDetailTitle';
 import FeaturedImage from 'parts/FeaturedImage';
 import PageDetailDescription from 'parts/PageDetailDescription';
 import BookingForm from 'parts/BookingForm';
-import Categories from 'parts/Categories';
-import Testimony from 'parts/Testimony';
-import Footer from 'parts/Footer';
 
 import ItemDetails from 'json/itemDetails';
 
@@ -26,26 +21,22 @@ export default class DetailsPage extends Component {
     ];
     return (
       <>
-        <Header {...this.props} />
-        <PageDetailTitle breadcrumb={breadcrumbList} data={ItemDetails} />
+        <Header {...this.props}></Header>
+        <PageDetailTitle
+          breadcrumb={breadcrumbList}
+          data={ItemDetails}
+        ></PageDetailTitle>
         <FeaturedImage data={ItemDetails.imageUrls} />
         <section className="container">
           <div className="row">
             <div className="col-7 pr5">
-              <Fade bottom>
-                <PageDetailDescription data={ItemDetails} />
-              </Fade>
+              <PageDetailDescription data={ItemDetails} />
             </div>
             <div className="col-5">
-              <Fade bottom>
-                <BookingForm itemDetails={ItemDetails} />
-              </Fade>
+              <BookingForm itemDetails={ItemDetails} />
             </div>
           </div>
         </section>
-        <Categories data={ItemDetails.categories} />
-        <Testimony data={ItemDetails.testimonial} />
-        <Footer />
       </>
     );
   }
