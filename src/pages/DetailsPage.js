9ec9e278f745c178014899abbcf90e34b2f29676
Roman Zidan Ramadhan <romanzidanramadhan@gmail.com>
2020-05-05 23:59:02 +0800

    Update Details Page and fix
    Added Categories
    Added Testiomny
    Added Footer
    Added transition fade on Booking Form and Description

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade';
 
 import Header from 'parts/Header';
 import PageDetailTitle from 'parts/PageDetailTitle';
 import FeaturedImage from 'parts/FeaturedImage';
 import PageDetailDescription from 'parts/PageDetailDescription';
 import BookingForm from 'parts/BookingForm';
+import Categories from 'parts/Categories';
+import Testimony from 'parts/Testimony';
+import Footer from 'parts/Footer';
 
 import ItemDetails from 'json/itemDetails';
 
@@ -30,13 +34,20 @@ export default class DetailsPage extends Component {
         <section className="container">
           <div className="row">
             <div className="col-7 pr5">
-              <PageDetailDescription data={ItemDetails} />
+              <Fade bottom>
+                <PageDetailDescription data={ItemDetails} />
+              </Fade>
             </div>
             <div className="col-5">
-              <BookingForm itemDetails={ItemDetails} />
+              <Fade bottom>
+                <BookingForm itemDetails={ItemDetails} />
+              </Fade>
             </div>
           </div>
         </section>
+        <Categories data={ItemDetails.categories} />
+        <Testimony data={ItemDetails.testimonial} />
+        <Footer />
       </>
     );
   }
